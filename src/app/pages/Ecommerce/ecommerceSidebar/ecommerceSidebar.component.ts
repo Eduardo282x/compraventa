e08fb26ce357c3f37fe7b6b90142e93296c4f562
UgazPro <ugazdesu@gmail.com>
2025-02-03 15:33:21 -0400

    sidebar

---
+++ src/app/pages/Ecommerce/ecommerceSidebar/ecommerceSidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ecommerce-sidebar',
+  imports: [],
+  templateUrl: './ecommerceSidebar.component.html',
+  styleUrl: './ecommerceSidebar.component.css',
+})
+export class EcommerceSidebarComponent { }
