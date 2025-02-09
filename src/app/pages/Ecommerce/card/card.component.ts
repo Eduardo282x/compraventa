2f75f4caba7463d94bf05cbc09992afeb3a04565
Eduardo282x <eduardokunmixyeah@gmail.com>
2025-02-09 16:38:26 -0400

    fix login normal, add carrito

--- src/app/pages/Ecommerce/card/card.component.ts
+++ src/app/pages/Ecommerce/card/card.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { IInventario } from '../../../interfaces/producto.interface';
 import { CurrencyPipe } from '@angular/common';
+import { CarritoService } from '../../../services/carrito.service';
 
 @Component({
   selector: 'app-card',
@@ -11,4 +12,20 @@ import { CurrencyPipe } from '@angular/common';
 })
 export class CardComponent { 
   @Input() product: IInventario = {} as IInventario;
+  carritoService = inject(CarritoService);
+
+  addCarrito(product: IInventario) {
+    const getCarritoLocal: number[] = JSON.parse(localStorage.getItem('carrito') as string);
+
+    if(!getCarritoLocal){
+      localStorage.setItem('carrito', JSON.stringify([product.prodId]))
+    }
+
+    const addProductCarrito: number[] = [...getCarritoLocal]
+    addProductCarrito.push(product.prodId);
+
+    localStorage.setItem('carrito', JSON.stringify(addProductCarrito))
+    this.carritoService.setCarrito.set(addProductCarrito);
+
+  }
 }
--- src/app/pages/Ecommerce/ecommerceHeader/ecommerceHeader.component.ts
+++ src/app/pages/Ecommerce/ecommerceHeader/ecommerceHeader.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { ChangeDetectorRef, Component, effect, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+
+import { CarritoService } from '../../../services/carrito.service';
 @Component({
   selector: 'app-ecommerce-header',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, MatBadgeModule],
   templateUrl: './ecommerceHeader.component.html',
   styleUrl: './ecommerceHeader.component.css',
 })
-export class EcommerceHeaderComponent { }
+export class EcommerceHeaderComponent {
+
+  carritoService = inject(CarritoService);
+  ref = inject(ChangeDetectorRef);
+
+  articlesCarrito = 0;
+
+  constructor() {
+    effect(() => {
+      this.articlesCarrito = this.carritoService.getCarrito().length;
+      this.ref.detectChanges();
+    })
+  }
+
+}
