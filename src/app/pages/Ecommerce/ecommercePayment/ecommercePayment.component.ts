ba41e3fb43fc8453ae731c9dbc216b5da9f2ef3b
UgazPro <ugazdesu@gmail.com>
2025-02-15 13:30:51 -0400

    Payment data

---
+++ src/app/pages/Ecommerce/ecommercePayment/ecommercePayment.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ecommerce-payment',
+  imports: [],
+  templateUrl: './ecommercePayment.component.html',
+  styleUrl: './ecommercePayment.component.css',
+})
+export class EcommercePaymentComponent { }
